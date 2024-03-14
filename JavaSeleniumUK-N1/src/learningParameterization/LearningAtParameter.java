22285006b60e7d2498e83a6837f30b8734666644
Sumanth Chinivar <Sumanth Chinivar@TYSS-Sumanth-Chinivar>
2024-03-14 17:56:19 +0530

    Adding changes to Learning parameter class

--- JavaSeleniumUK-N1/src/learningParameterization/LearningAtParameter.java
+++ JavaSeleniumUK-N1/src/learningParameterization/LearningAtParameter.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class LearningAtParameter {
-	
+	//Learning parameterization 
 	@Test
 	@Parameters({"a","b"})
 	public void add(@Optional("10") int a,@Optional("20") int b)
