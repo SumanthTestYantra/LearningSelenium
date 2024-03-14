832178737cf9edbd1e6ed6f5cec153e8cd49ad4b
SumanthTestYantra <163341693+SumanthTestYantra@users.noreply.github.com>
2024-03-14 18:43:23 +0530

    Update HomePage.java

--- JavaSeleniumUK-N1/src/learningPOM/HomePage.java
+++ JavaSeleniumUK-N1/src/learningPOM/HomePage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+//adding a comments
+
 public class HomePage {
 
 	public HomePage(WebDriver driver) {
